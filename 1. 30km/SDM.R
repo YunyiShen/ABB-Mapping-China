2e7ff8b97c7b8d278c95276065e815897da05ee5
YunyiShen <yshen99@wisc.edu>
2020-02-13 20:21:39 -0600

    add cross validation

--- 1. 30km/SDM.R
+++ 1. 30km/SDM.R
@@ -2,8 +2,16 @@ workdir <- "/1. 30km"
 setwd(paste0(getwd(),workdir))
 allPA <- read.csv(list.files(pattern = ".csv$"))
 
+
+
 require(raster)
 require(biomod2)
+require(caret)
+
+folds = createFolds(y=data_[,response]
+                       ,k=fold) # create a fold
+
+
 
 pathfac <- paste0(getwd(),'/rasters/dis')
 pathcon <- paste0(getwd(),'/rasters/cts')
@@ -26,41 +34,52 @@ my.stack <- stack(stack1,stack2)
 rm(stack1)
 rm(stack2)
 
-distributiondata.species1 <- 
-  BIOMOD_FormatingData(
-    resp.var = as.numeric(allPA[,5]),
-    expl.var = my.stack,
-    resp.xy = (allPA[, 3:4]),
-    resp.name = 'bear'
-  )
+
+set.seed(42)
+fold = 5
+folds = createFolds(y=allPA[,5],k=fold) # create a fold
+res = list()
+
+for(i in 1:fold){
+	distributiondata.species1 <- 
+  		BIOMOD_FormatingData(
+    		resp.var = as.numeric(allPA[-folds[[i]],5]),
+    		expl.var = my.stack,
+    		resp.xy = (allPA[-folds[[i]],3:4]),
+    		resp.name = paste0("bear",i)
+  		)
 
 
-SDM.1 <-
-  BIOMOD_Modeling(
-    data = distributiondata.species1,
-    models = 'RF',
-    DataSplit=85,
-    models.eval.meth = c('TSS', 'ROC') ,
-    VarImport = 3,
-    SaveObj = T
-  )
+		SDM.1 <-
+  			BIOMOD_Modeling(
+    		data = distributiondata.species1,
+    		models = 'RF',
+    		DataSplit=1,
+    		models.eval.meth = c('TSS', 'ROC') ,
+    		VarImport = 3,
+    		SaveObj = T
+  		)
 
-distribution.est.1.full <- BIOMOD_Projection(SDM.1, my.stack, 'distribution1.est',build.clamping.mask = F,omit.na = F)
-plot(distribution.est.1.full)
-distribution.est.1 <- distribution.est.1.full@proj@val[[2]]
-plot(distribution.est.1)
-writeRaster(distribution.est.1,paste0((getwd()),"bear_bio1_12.tif"),overwrite=T)
-summary(SDM.1)
+		distribution.est.1.full <- BIOMOD_Projection(SDM.1, my.stack, 'distribution1.est',build.clamping.mask = F,omit.na = F)
+		#plot(distribution.est.1.full)
+		distribution.est.1 <- distribution.est.1.full@proj@val[[2]]
+		#plot(distribution.est.1)
+		writeRaster(distribution.est.1,paste0((getwd()),"/low/bear_1.12_fold_",i,".tif"),overwrite=T)
+		#distribution.est.1 = raster(file.choose())
+		res[[i]] = list(model_high = SDM.1,prediction_low = distribution.est.1,val = allPA[folds[[i]],])
+		stack.all = stack(distribution.est.1,my.stack)
 
-stack.all = stack(distribution.est.1,my.stack)
+		proj_env = data.frame( na.omit(getValues(stack.all)))
+		rm(stack.all)
+		env_name = colnames(proj_env)
 
-proj_env = data.frame( na.omit(getValues(stack.all)))
-rm(stack.all)
-env_name = colnames(proj_env)
-
-for(i in 2:13){
-  jpeg(filename=paste0(env_name[i],".jpg"))
-  plot(proj_env[,i],proj_env[,1])
-  dev.off()
+		for(i in 2:ncol(proj_env)){
+  			jpeg(filename=paste0(env_name[i],"_fold_",i,".jpg"))
+  			plot(proj_env[,i],proj_env[,1])
+  			dev.off()
+		}
+	
+	
 }
+
 
--- 2. 3.16km/SDM.R
+++ 2. 3.16km/SDM.R
@@ -4,6 +4,7 @@ allPA <- read.csv(list.files(pattern = ".csv$"))
 
 require(raster)
 require(biomod2)
+require(caret)
 
 pathfac <- paste0(getwd(),'/rasters/dis')
 pathcon <- paste0(getwd(),'/rasters/cts')
@@ -24,40 +25,49 @@ my.stack <- stack(stack1,stack2)
 my.stack = my.stack[[-2]]
 #my.stack = my.stack[[-3]]
 
+set.seed(114514)
 rm(stack1)
 rm(stack2)
+fold = 5
+folds = createFolds(y=allPA[,5],k=fold) # create a fold
+res = list()
 
-distributiondata.species1 <- 
-  BIOMOD_FormatingData(
-    resp.var = as.numeric(allPA[,5]),
-    expl.var = my.stack,
-    resp.xy = (allPA[, 3:4]),
-    resp.name = 'bear'
-  )
+for(i in 1:fold){
+	distributiondata.species1 <- 
+  		BIOMOD_FormatingData(
+    		resp.var = as.numeric(allPA[-folds[[i]],5]),
+    		expl.var = my.stack,
+    		resp.xy = (allPA[-folds[[i]],3:4]),
+    		resp.name = paste0("bear",i)
+  		)
 
 
-SDM.1 <-
-  BIOMOD_Modeling(
-    data = distributiondata.species1,
-    models = 'RF',
-    DataSplit=85,
-    models.eval.meth = c('TSS', 'ROC') ,
-    VarImport = 3,
-    SaveObj = T
-  )
+		SDM.1 <-
+  			BIOMOD_Modeling(
+    		data = distributiondata.species1,
+    		models = 'RF',
+    		DataSplit=1,
+    		models.eval.meth = c('TSS', 'ROC') ,
+    		VarImport = 3,
+    		SaveObj = T
+  		)
 
-distribution.est.1.full <- BIOMOD_Projection(SDM.1, my.stack, 'distribution1.est',build.clamping.mask = F,omit.na = F)
-plot(distribution.est.1.full)
-distribution.est.1 <- distribution.est.1.full@proj@val[[2]]
-plot(distribution.est.1)
-writeRaster(distribution.est.1,paste0((getwd()),"bear_3.16.tif"),overwrite=T)
-summary(SDM.1)
+		distribution.est.1.full <- BIOMOD_Projection(SDM.1, my.stack, 'distribution1.est',build.clamping.mask = F,omit.na = F)
+		#plot(distribution.est.1.full)
+		distribution.est.1 <- distribution.est.1.full@proj@val[[2]]
+		#plot(distribution.est.1)
+		writeRaster(distribution.est.1,paste0((getwd()),"/high/bear_1.12_fold_",i,".tif"),overwrite=T)
+		#distribution.est.1 = raster(file.choose())
+		LR = raster(paste0(getwd()),"/low/bear_1.12_fold",i,".tif"))
+		L2R = resample(LR,distribution.est.1)
 
-distribution.est.1 = raster(file.choose())
-LR = raster(file.choose())
-L2R = resample(LR,distribution.est.1)
+		#writeRaster(L2R,file.choose())
 
-writeRaster(L2R,file.choose())
+		combined = ((distribution.est.1/1000)*(L2R/1000))/((1-distribution.est.1/1000)*(1-L2R/1000)+(distribution.est.1/1000)*(L2R/1000))
+		writeRaster(combined,paste0(getwd()),"/combine/bear_1.12_fold",i,".tif"),overwrite=T)
+		write.csv(allPA[folds[[i]],],paste0("./high/val_bear_1.12_fold_",i,".csv"))
+		res[[i]] = list(model_high = SDM.1,prediction_high = distribution.est.1,val = allPA[folds[[i]],],fusion = combined)
+	
+	
+}
 
-combined = ((distribution.est.1/1000)*(L2R/1000))/((1-distribution.est.1/1000)*(1-L2R/1000)+(distribution.est.1/1000)*(L2R/1000))
-writeRaster(combined,file.choose(),overwrite=T)
